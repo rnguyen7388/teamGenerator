73faa6e0575e8919d8290811c1a532d67ab7a2ed
Ray <rnguyen7388@gmail.com>
2020-07-19 01:31:01 -0500

    fixed adding issue

--- app.js
+++ app.js
@@ -4,80 +4,84 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
-const { listenerCount } = require("process");
-const Employee = require("./lib/Employee");
-
 const teamMembers = [];
 
 const teamQuestions = () => {
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                message: "Choose type of team member to add",
-                name: "memberChoice",
-                choices: ["Manager", "Engineer", "Intern"]
-            },
-            {
-                type: "input",
-                message: "Enter Name:",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "enter ID:",
-                name: "id"
-            },
-            {
-                tpye: "input",
-                message: "Enter Email:",
-                name: "email"
-            }
-        ]).then(response => {
-            let members = {
-                Manager: "Office Number:",
-                Engineer: "GitHub:",
-                Intern: "School:"
-            }
-            inquirer
-                .prompt({
-                    type: "input",
-                    message: members[response.memberChoice],
-                    name: "member"
-                }).then(response => {
-                    let constructors = {
-                        Manager: Manager,
-                        Engineer: Engineer,
-                        Intern: Intern
-                    }
-                    const People = constructors[response.memberChoice];
-                    let newMember = new People(response.name, response.id, response.email, response.member)
-                    teamMembers.push(newMember);
-                    return;
-                }) .then(() => {
-                    inquirer
-                        .prompt({
-                            type: "confirm",
-                            message: "continue",
-                            name: "continue"
-                        }).then(response => {
-                            if (response.continue) {
-                                teamQuestions();
-                            } else {
-                                let html = render(employee);
-                                fs.writeFile(outputPath, html, (err) => {
-                                    if (err) throw err;
-                                })
-                            }
-                        })
-                })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Choose type of team member to add",
+        name: "memberChoice",
+        choices: ["Manager", "Engineer", "Intern"]
+      },
+      {
+        type: "input",
+        message: "Enter Name:",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "Enter ID:",
+        name: "id"
+      },
+      {
+        tpye: "input",
+        message: "Enter Email:",
+        name: "email"
+      },
+    ])
+    .then((response) => {
+      let members = {
+        Manager: "Office Number:",
+        Engineer: "GitHub:",
+        Intern: "School:"
+      };
+      inquirer
+        .prompt({
+          type: "input",
+          message: members[response.memberChoice],
+          name: "member"
         })
-}
-
+        .then((res) => {
+          let constructors = {
+            Manager: Manager,
+            Engineer: Engineer,
+            Intern: Intern
+          };
+          const questions = constructors[response.memberChoice];
+          let newMember = new questions(
+            response.name,
+            response.id,
+            response.email,
+            res.member
+          );
+          teamMembers.push(newMember);
+          return;
+        })
+        .then(() => {
+          inquirer
+            .prompt({
+              type: "confirm",
+              message: "Would you like to add another member?",
+              name: "confirm"
+            })
+            .then((response) => {
+              if (response.confirm) {
+                teamQuestions();
+              } else {
+                let html = render(teamMembers);
+                fs.writeFile(outputPath, html, (err) => {
+                  if (err) throw err;
+                });
+              }
+            });
+        });
+    });
+};
 teamQuestions();
-        
